ceb4741ff2fc25a0ed459eb667c699e4204e359a
Sreejarai123 <sreeja.t.rai@gmail.com>
2024-02-15 08:40:25 -0800

    improved

--- _site/script.js
+++ _site/script.js
@@ -1,166 +1,153 @@
+// game.js
 
-let gameStart=true;
-
-
-// document.addEventListener("keypress",function(event){
-//   console.log("inside first if condition");
-//   if(event.key=="Enter"){
-//     console.log("Enter was pressed");
-//     gameStart=true;
-//     requestAnimationFrame(gameLoop);
-//   }
-  
-// });
-
-
-    if(localStorage.getItem("maxScore")!=undefined){
-      alert("Rod 1 has maximum score of "+localStorage.getItem(("maxScore")));
-    }
-    else{
-      alert("this is your first time");
-    }
-
-
-let rodTop=document.getElementById("rod-top");
-let rodBottom=document.getElementById("rod-bottom");
-let ball=document.getElementById("ball");
-let gameContainer=document.getElementById("game-container");
-let aPressed=false;
-let dPressed=false;
-let score=0;
-let gameStart = false;
 let gameRunning = false;
+let rodTop = document.getElementById("rod-top") || createRod("rod-top");
+let rodBottom = document.getElementById("rod-bottom") || createRod("rod-bottom");
+let ball = document.getElementById("ball") || createBall();
+let gameContainer = document.getElementById("game-container");
+let aPressed = false;
+let dPressed = false;
+let score = 0;
+let Vx = -1;
+let Vy = -5;
+let Vel = Math.sqrt(Math.pow(Vx, 2) + Math.pow(Vy, 2));
 
+// Create start and stop buttons dynamically if not already present
+const startButton = document.getElementById("start-button") || createButton("Start", startGame, "start-button");
+const stopButton = document.getElementById("stop-button") || createButton("Stop", stopGame, "stop-button");
 
-// document.addEventListener("keypress",function(){
-//   console.log("clicked");
-// });
-
-document.addEventListener("keypress",rightKeyHandler);
-document.addEventListener("keyup",leftKeyHandler);
-
-function rightKeyHandler(event){
-  if(event.key=="d"){
-    dPressed=true;
-    console.log("d pressed");
-  }
-  else if(event.key=="a"){
-    aPressed=true;
-    console.log("a pressed");
-  }
+if (!document.getElementById("start-button")) {
+    document.body.appendChild(startButton);
 }
 
-function leftKeyHandler(event){
-  if(event.key=="d"){
-    dPressed=false;
-    console.log("d released");
-  }
-  else if(event.key=="a"){
-    aPressed=false;
-    console.log("a released");
-  }
+if (!document.getElementById("stop-button")) {
+    document.body.appendChild(stopButton);
 }
 
-// For the velocity of the ball it is 2d, it will have a x component and a y component
-// we will update the position of the ball by adding the velocity to the position of the ball
-//we will also check if the ball is hitting the wall or rod, it will change the direction of the ball.
-// The formula for the same would be v2=vx2+vy2;
+document.addEventListener("keydown", keyDownHandler);
+document.addEventListener("keyup", keyUpHandler);
 
-if(gameStart){
-let Vx=-1;
-let Vy=-5;
-let Vel=Math.sqrt(Math.pow(Vx,2)+Math.pow(Vy,2));
-
-function playAgain(){
-  ball.style.left="50%";
-  ball.style.top="50%";
-  Vx=-1;
-  Vy=-5;
-  Vel=Math.sqrt(Math.pow(Vx,2)+Math.pow(Vy,2));
+function createButton(text, clickHandler, id) {
+    const button = document.createElement("button");
+    button.textContent = text;
+    button.addEventListener("click", clickHandler);
+    button.id = id;
+    return button;
 }
 
-function checkCollission(activepaddle){
-      let balltop=ball.offsetTop;
-      let ballbottom=ball.offsetTop+ball.offsetHeight;
-      let ballleft=ball.offsetLeft;
-      let ballright=ball.offsetLeft+ball.offsetWidth;
-
-      let paddletop=activepaddle.offsetTop;
-      let paddlebottom=activepaddle.offsetTop+activepaddle.offsetHeight;
-      let paddleleft=activepaddle.offsetLeft;
-      let paddleright=activepaddle.offsetLeft+activepaddle.offsetWidth;
-
-      // console.log(balltop,ballbottom,ballleft,ballright);
-
-      // console.log(paddlebottom,paddletop,paddleleft,paddleright);
-      if(ballbottom>paddletop && balltop<paddlebottom && ballright>paddleleft && ballleft<paddleright){
-        // console.log("collision detected");
-        return true;
-      }
-       else return false;
-}    
-
-function gameLoop(){
-  if (gameRunning) {
-  if(ball.offsetLeft<0){
-    Vx=-Vx;
-  }
-  if(ball.offsetLeft>gameContainer.offsetWidth-ball.offsetWidth){
-    Vx=-Vx;
-  }
-  if(ball.offsetTop<0){
-    if(score>localStorage.getItem("maxScore"))
-    localStorage.setItem("maxScore",score);
-
-    playAgain();
+function createRod(id) {
+    const rod = document.createElement("div");
+    rod.id = id;
+    // Set other styles and properties for the rod
+    return rod;
 }
-  if(ball.offsetTop>gameContainer.offsetHeight-ball.offsetHeight){
-    if(score>localStorage.getItem("maxScore"))
-    localStorage.setItem("maxScore",score);
-    playAgain();
-  }
-  let rod=ball.offsetTop>gameContainer.offsetHeight/2?rodBottom:rodTop;
-  let ballcenterX=ball.offsetLeft+ball.offsetWidth/2;
-  let paddlecenterX=rod.offsetLeft+rod.offsetWidth/2;
-  let angle=0;
-  if(checkCollission(rod)){
-    score++;
-    // console.log("inside checkcollision function")
-    if(rod==rodTop){
-        
-        if(ballcenterX<paddlecenterX){
-          angle=3*Math.PI/4;
-        }
-        else if(ballcenterX>paddlecenterX){
-          angle=Math.PI/4;
-        }
-        else angle=0;
+
+function createBall() {
+    const ball = document.createElement("div");
+    ball.id = "ball";
+    // Set other styles and properties for the ball
+    return ball;
+}
+
+function startGame() {
+    if (!gameRunning) {
+        gameRunning = true;
+        playAgain(); // Reset the ball position before starting
+        requestAnimationFrame(gameLoop);
     }
-    else if(rod==rodBottom){
-      if(ballcenterX<paddlecenterX){
-        angle=-3*Math.PI/4;
-      }
-      else if(ballcenterX>paddlecenterX){
-        angle=-Math.PI/4;
-      }
-      else angle=Math.PI/2;
-    }
-    Vel=Vel+0.2;
-    Vx=Vel * Math.cos(angle);
-    Vy=Vel * Math.sin(angle);
-  }
-
-  ball.style.top=ball.offsetTop+Vy+"px";
-  ball.style.left=ball.offsetLeft+Vx+"px";
-  if(aPressed && rodTop.offsetLeft>1){
-      rodTop.style.left=rodTop.offsetLeft-4+"px";
-      rodBottom.style.left=rodBottom.offsetLeft-4+"px";
-  }
-  if(dPressed && rodTop.offsetLeft<gameContainer.offsetWidth-rodTop.offsetWidth){
-    rodTop.style.left=rodTop.offsetLeft+4+"px";
-    rodBottom.style.left=rodBottom.offsetLeft+4+"px";
-  }
-  requestAnimationFrame(gameLoop);
 }
-gameLoop();
+
+function stopGame() {
+    gameRunning = false;
+}
+
+function keyDownHandler(event) {
+    if (event.key == "d") {
+        dPressed = true;
+    } else if (event.key == "a") {
+        aPressed = true;
+    }
+}
+
+function keyUpHandler(event) {
+    if (event.key == "d") {
+        dPressed = false;
+    } else if (event.key == "a") {
+        aPressed = false;
+    }
+}
+
+function movePaddles() {
+    if (aPressed && rodTop.offsetLeft > 1) {
+        rodTop.style.left = rodTop.offsetLeft - 4 + "px";
+        rodBottom.style.left = rodBottom.offsetLeft - 4 + "px";
+    }
+    if (dPressed && rodTop.offsetLeft < gameContainer.offsetWidth - rodTop.offsetWidth) {
+        rodTop.style.left = rodTop.offsetLeft + 4 + "px";
+        rodBottom.style.left = rodBottom.offsetLeft + 4 + "px";
+    }
+}
+
+function playAgain() {
+    ball.style.left = "50%";
+    ball.style.top = "50%";
+    Vx = -1;
+    Vy = -5;
+    Vel = Math.sqrt(Math.pow(Vx, 2) + Math.pow(Vy, 2));
+}
+
+function checkCollision(activePaddle) {
+    let ballTop = ball.offsetTop;
+    let ballBottom = ball.offsetTop + ball.offsetHeight;
+    let ballLeft = ball.offsetLeft;
+    let ballRight = ball.offsetLeft + ball.offsetWidth;
+
+    let paddleTop = activePaddle.offsetTop;
+    let paddleBottom = activePaddle.offsetTop + activePaddle.offsetHeight;
+    let paddleLeft = activePaddle.offsetLeft;
+    let paddleRight = activePaddle.offsetLeft + activePaddle.offsetWidth;
+
+    return ballBottom > paddleTop && ballTop < paddleBottom && ballRight > paddleLeft && ballLeft < paddleRight;
+}
+
+function gameLoop() {
+    if (gameRunning) {
+        if (ball.offsetLeft < 0 || ball.offsetLeft > gameContainer.offsetWidth - ball.offsetWidth) {
+            Vx = -Vx;
+        }
+
+        if (ball.offsetTop < 0 || ball.offsetTop > gameContainer.offsetHeight - ball.offsetHeight) {
+            if (score > localStorage.getItem("maxScore")) {
+                localStorage.setItem("maxScore", score);
+            }
+
+            playAgain();
+        }
+
+        let rod = ball.offsetTop > gameContainer.offsetHeight / 2 ? rodBottom : rodTop;
+        let ballcenterX = ball.offsetLeft + ball.offsetWidth / 2;
+        let paddlecenterX = rod.offsetLeft + rod.offsetWidth / 2;
+        let angle = 0;
+
+        if (checkCollision(rod)) {
+            score++;
+
+            if (rod == rodTop) {
+                angle = ballcenterX < paddlecenterX ? 3 * Math.PI / 4 : ballcenterX > paddlecenterX ? Math.PI / 4 : 0;
+            } else if (rod == rodBottom) {
+                angle = ballcenterX < paddlecenterX ? -3 * Math.PI / 4 : ballcenterX > paddlecenterX ? -Math.PI / 4 : Math.PI / 2;
+            }
+
+            Vel = Vel + 0.2;
+            Vx = Vel * Math.cos(angle);
+            Vy = Vel * Math.sin(angle);
+        }
+
+        ball.style.top = ball.offsetTop + Vy + "px";
+        ball.style.left = ball.offsetLeft + Vx + "px";
+
+        movePaddles();
+
+        requestAnimationFrame(gameLoop);
+    }
 }
